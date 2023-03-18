e33684d7e400e14713e217f04a612a35192833c6
Leonardo Tapia <leonardo.tapia0753@alumnos.udg.mx>
2023-03-17 20:00:32 -0600

    Integración de interfaz gráfica con Aeropuerto

--- mainwindow.py
+++ mainwindow.py
@@ -1,16 +1,44 @@
 from PySide2.QtWidgets import QMainWindow
 from PySide2.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
+from aeropuerto import Aeropuerto
+from vuelo import Vuelo
 
 class MainWindow(QMainWindow):
-    def __init__(self): # Reservar memoria para mostrar ventana
-        super(MainWindow, self).__init__() # Llama al constructor de QMainWindow para reservar memoria
-        # Lo anterior es una clase hija
-        ui = Ui_MainWindow()
-        ui.setupUi(self)
+    def __init__(self):
+        super(MainWindow, self).__init__()
 
-        ui.pushButton.clicked.connect(self.click_agregar)
+        self.aeropuerto = Aeropuerto()
+
+        self.ui  = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.agregar_inicio_pushButton.clicked.connect(self.click_agregar_inicio)
+        self.ui.agregar_final_pushButton.clicked.connect(self.click_agregar_final)
+        self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
 
     @Slot( )
-    def click_agregar(self):
-        print("click")
+    def click_agregar_inicio(self):
+        id = self.ui.id_lineEdit.text()
+        origen = self.ui.origen_lineEdit.text()
+        destino = self.ui.destino_lineEdit.text()
+        peso = self.ui.peso_spinBox.value()
+
+        vuelo = Vuelo(id, origen, destino, peso)
+        self.aeropuerto.agregar_inicio(vuelo)
+
+    @Slot( )
+    def click_agregar_final(self):
+        id = self.ui.id_lineEdit.text()
+        origen = self.ui.origen_lineEdit.text()
+        destino = self.ui.destino_lineEdit.text()
+        peso = self.ui.peso_spinBox.value()
+
+        vuelo = Vuelo(id, origen, destino, peso)
+        self.aeropuerto.agregar_final(vuelo)
+
+    @Slot( )
+    def click_mostrar(self):
+        self.ui.salida.clear()
+        self.ui.salida.insertPlainText(str(self.aeropuerto))
+
