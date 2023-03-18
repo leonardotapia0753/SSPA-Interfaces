dd1a8b7552ddacab0445a4b0637bce433f7be6f5
Leonardo Tapia <leonardo.tapia0753@alumnos.udg.mx>
2023-03-17 20:01:01 -0600

    Clase Vuelo y Aeropuerto creadas

---
+++ vuelo.py
@@ -0,0 +1,14 @@
+class Vuelo:
+    def __init__(self, id = "", origen = "", destino = "", peso = 0):
+        self.__id = id
+        self.__origen = origen
+        self.__destino = destino
+        self.__peso = peso
+
+    def __str__(self):
+        return(
+            "Identificador: " + self.__id + "\n" +
+            "Origen: " + self.__origen + "\n" +
+            "Destino: " + self.__destino + "\n" +
+            "Peso: " + str(self.__peso) + "\n"
+        )
