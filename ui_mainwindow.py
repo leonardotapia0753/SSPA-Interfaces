8ab41e309ecd050938fcb511ce35a5486b9e7043
Leonardo Tapia <leonardo.tapia0753@alumnos.udg.mx>
2023-03-18 21:44:52 -0600

    Diseño de ventana actualizado

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -18,6 +18,10 @@ class Ui_MainWindow(object):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(600, 500)
+        self.actionAbrir = QAction(MainWindow)
+        self.actionAbrir.setObjectName(u"actionAbrir")
+        self.actionGuardar = QAction(MainWindow)
+        self.actionGuardar.setObjectName(u"actionGuardar")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayout_5 = QHBoxLayout(self.centralwidget)
@@ -100,17 +104,17 @@ class Ui_MainWindow(object):
         self.widget.setSizePolicy(sizePolicy1)
         self.verticalLayout_3 = QVBoxLayout(self.widget)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.agregar_inicio_pushButton = QPushButton(self.widget)
-        self.agregar_inicio_pushButton.setObjectName(u"agregar_inicio_pushButton")
-        self.agregar_inicio_pushButton.setTabletTracking(False)
-
-        self.verticalLayout_3.addWidget(self.agregar_inicio_pushButton)
-
         self.agregar_final_pushButton = QPushButton(self.widget)
         self.agregar_final_pushButton.setObjectName(u"agregar_final_pushButton")
         self.agregar_final_pushButton.setTabletTracking(False)
 
         self.verticalLayout_3.addWidget(self.agregar_final_pushButton)
+
+        self.agregar_inicio_pushButton = QPushButton(self.widget)
+        self.agregar_inicio_pushButton.setObjectName(u"agregar_inicio_pushButton")
+        self.agregar_inicio_pushButton.setTabletTracking(False)
+
+        self.verticalLayout_3.addWidget(self.agregar_inicio_pushButton)
 
         self.mostrar_pushButton = QPushButton(self.widget)
         self.mostrar_pushButton.setObjectName(u"mostrar_pushButton")
@@ -142,10 +146,16 @@ class Ui_MainWindow(object):
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 600, 22))
+        self.menuArchivo = QMenu(self.menubar)
+        self.menuArchivo.setObjectName(u"menuArchivo")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuArchivo.menuAction())
+        self.menuArchivo.addAction(self.actionAbrir)
+        self.menuArchivo.addAction(self.actionGuardar)
 
         self.retranslateUi(MainWindow)
 
@@ -154,14 +164,23 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionAbrir.setText(QCoreApplication.translate("MainWindow", u"Abrir", None))
+#if QT_CONFIG(shortcut)
+        self.actionAbrir.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+O", None))
+#endif // QT_CONFIG(shortcut)
+        self.actionGuardar.setText(QCoreApplication.translate("MainWindow", u"Guardar", None))
+#if QT_CONFIG(shortcut)
+        self.actionGuardar.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+S", None))
+#endif // QT_CONFIG(shortcut)
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Vuelo", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"id:", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Origen:", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"Detino:", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Peso:", None))
-        self.agregar_inicio_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar Inicio", None))
         self.agregar_final_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar Final", None))
+        self.agregar_inicio_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar Inicio", None))
         self.mostrar_pushButton.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
         self.salida.setPlainText("")
+        self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
     # retranslateUi
 
