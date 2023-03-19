e875aa3caa63fa8190d24fe479f88b3073b1c9e3
Leonardo Tapia <leonardo.tapia0753@alumnos.udg.mx>
2023-03-18 21:39:39 -0600

    Método de conversión a diccionario agregado

--- vuelo.py
+++ vuelo.py
@@ -12,3 +12,11 @@ class Vuelo:
             "Destino: " + self.__destino + "\n" +
             "Peso: " + str(self.__peso) + "\n"
         )
+    
+    def to_dict(self):
+        return {
+            "id": self.__id,
+            "origen": self.__origen,
+            "destino": self.__destino,
+            "peso": self.__peso
+        }
