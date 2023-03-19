f6e55aae6e3aeca4fed4b5d2ed413f194bee2a69
Leonardo Tapia <leonardo.tapia0753@alumnos.udg.mx>
2023-03-18 21:44:21 -0600

    Integración de nuevos elementos de ventana

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox
 from PySide2.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
 from aeropuerto import Aeropuerto
@@ -16,6 +16,53 @@ class MainWindow(QMainWindow):
         self.ui.agregar_inicio_pushButton.clicked.connect(self.click_agregar_inicio)
         self.ui.agregar_final_pushButton.clicked.connect(self.click_agregar_final)
         self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
+        self.ui.actionAbrir.triggered.connect(self.action_Abrir_Archivo)
+        self.ui.actionGuardar.triggered.connect(self.action_Guardar_Archivo)
+
+
+    @Slot( )
+    def action_Abrir_Archivo(self):
+        #print("Abrir_Archivo")
+        ubicacion = QFileDialog.getOpenFileName(
+            self,
+            'Abrir Archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        if self.aeropuerto.abrir(ubicacion)
+            QMessageBox.information(
+                self,
+                "Éxito",
+                "Se pudo abrir el archivo " + ubicacion
+            )
+        else:
+            QMessageBox.information(
+                self,
+                "Error",
+                "No se pudo abrir el archivo " + ubicacion
+            )
+
+    @Slot( )
+    def action_Guardar_Archivo(self):
+        ubicacion = QFileDialog.getSaveFileName(
+            self,
+            'Guardar Archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        print(ubicacion)
+        if self.aeropuerto.guardar(ubicacion):
+            QMessageBox.information(
+                self,
+                "Éxito",
+                "Se pudo crear el archivo " + ubicacion
+            )
+        else:
+            QMessageBox.information(
+                self,
+                "Error",
+                "No se pudo crear el archivo " + ubicacion
+            )
 
     @Slot( )
     def click_agregar_inicio(self):
