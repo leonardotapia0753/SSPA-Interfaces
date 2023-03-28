0f1c36c1b5a7ffda928a127c1af2757f46aa2505
Leonardo Tapia <leonardo.tapia0753@alumnos.udg.mx>
2023-03-28 12:47:41 -0600

    Se agrego un tab para tabla de vuelos

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -17,135 +17,122 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(600, 500)
+        MainWindow.resize(613, 473)
         self.actionAbrir = QAction(MainWindow)
         self.actionAbrir.setObjectName(u"actionAbrir")
         self.actionGuardar = QAction(MainWindow)
         self.actionGuardar.setObjectName(u"actionGuardar")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.horizontalLayout_5 = QHBoxLayout(self.centralwidget)
-        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.groupBox = QGroupBox(self.centralwidget)
-        self.groupBox.setObjectName(u"groupBox")
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
-        self.groupBox.setSizePolicy(sizePolicy)
-        self.horizontalLayout = QHBoxLayout(self.groupBox)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.widget_3 = QWidget(self.groupBox)
-        self.widget_3.setObjectName(u"widget_3")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.widget_3.sizePolicy().hasHeightForWidth())
-        self.widget_3.setSizePolicy(sizePolicy1)
-        self.verticalLayout_4 = QVBoxLayout(self.widget_3)
-        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
-        self.widget_2 = QWidget(self.widget_3)
-        self.widget_2.setObjectName(u"widget_2")
-        sizePolicy1.setHeightForWidth(self.widget_2.sizePolicy().hasHeightForWidth())
-        self.widget_2.setSizePolicy(sizePolicy1)
-        self.gridLayout_3 = QGridLayout(self.widget_2)
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.gridLayout_3 = QGridLayout(self.tab)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.label = QLabel(self.widget_2)
-        self.label.setObjectName(u"label")
-
-        self.gridLayout_3.addWidget(self.label, 0, 0, 1, 1)
-
-        self.id_lineEdit = QLineEdit(self.widget_2)
+        self.groupBox = QGroupBox(self.tab)
+        self.groupBox.setObjectName(u"groupBox")
+        self.gridLayout = QGridLayout(self.groupBox)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.id_lineEdit = QLineEdit(self.groupBox)
         self.id_lineEdit.setObjectName(u"id_lineEdit")
-        self.id_lineEdit.setTabletTracking(False)
 
-        self.gridLayout_3.addWidget(self.id_lineEdit, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.id_lineEdit, 0, 1, 1, 1)
 
-        self.label_2 = QLabel(self.widget_2)
+        self.label_2 = QLabel(self.groupBox)
         self.label_2.setObjectName(u"label_2")
         self.label_2.setMinimumSize(QSize(0, 0))
 
-        self.gridLayout_3.addWidget(self.label_2, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
 
-        self.origen_lineEdit = QLineEdit(self.widget_2)
-        self.origen_lineEdit.setObjectName(u"origen_lineEdit")
-        self.origen_lineEdit.setTabletTracking(False)
-
-        self.gridLayout_3.addWidget(self.origen_lineEdit, 1, 1, 1, 1)
-
-        self.label_4 = QLabel(self.widget_2)
-        self.label_4.setObjectName(u"label_4")
-
-        self.gridLayout_3.addWidget(self.label_4, 2, 0, 1, 1)
-
-        self.destino_lineEdit = QLineEdit(self.widget_2)
-        self.destino_lineEdit.setObjectName(u"destino_lineEdit")
-        self.destino_lineEdit.setTabletTracking(False)
-
-        self.gridLayout_3.addWidget(self.destino_lineEdit, 2, 1, 1, 1)
-
-        self.label_3 = QLabel(self.widget_2)
+        self.label_3 = QLabel(self.groupBox)
         self.label_3.setObjectName(u"label_3")
 
-        self.gridLayout_3.addWidget(self.label_3, 3, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
 
-        self.peso_spinBox = QSpinBox(self.widget_2)
-        self.peso_spinBox.setObjectName(u"peso_spinBox")
-        self.peso_spinBox.setTabletTracking(False)
-
-        self.gridLayout_3.addWidget(self.peso_spinBox, 3, 1, 1, 1)
-
-
-        self.verticalLayout_4.addWidget(self.widget_2)
-
-        self.widget = QWidget(self.widget_3)
-        self.widget.setObjectName(u"widget")
-        sizePolicy1.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
-        self.widget.setSizePolicy(sizePolicy1)
-        self.verticalLayout_3 = QVBoxLayout(self.widget)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.agregar_final_pushButton = QPushButton(self.widget)
-        self.agregar_final_pushButton.setObjectName(u"agregar_final_pushButton")
-        self.agregar_final_pushButton.setTabletTracking(False)
-
-        self.verticalLayout_3.addWidget(self.agregar_final_pushButton)
-
-        self.agregar_inicio_pushButton = QPushButton(self.widget)
+        self.agregar_inicio_pushButton = QPushButton(self.groupBox)
         self.agregar_inicio_pushButton.setObjectName(u"agregar_inicio_pushButton")
-        self.agregar_inicio_pushButton.setTabletTracking(False)
 
-        self.verticalLayout_3.addWidget(self.agregar_inicio_pushButton)
+        self.gridLayout.addWidget(self.agregar_inicio_pushButton, 4, 1, 1, 1)
 
-        self.mostrar_pushButton = QPushButton(self.widget)
+        self.agregar_final_pushButton = QPushButton(self.groupBox)
+        self.agregar_final_pushButton.setObjectName(u"agregar_final_pushButton")
+
+        self.gridLayout.addWidget(self.agregar_final_pushButton, 5, 1, 1, 1)
+
+        self.label = QLabel(self.groupBox)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.destino_lineEdit = QLineEdit(self.groupBox)
+        self.destino_lineEdit.setObjectName(u"destino_lineEdit")
+
+        self.gridLayout.addWidget(self.destino_lineEdit, 2, 1, 1, 1)
+
+        self.mostrar_pushButton = QPushButton(self.groupBox)
         self.mostrar_pushButton.setObjectName(u"mostrar_pushButton")
-        self.mostrar_pushButton.setTabletTracking(False)
 
-        self.verticalLayout_3.addWidget(self.mostrar_pushButton)
+        self.gridLayout.addWidget(self.mostrar_pushButton, 6, 1, 1, 1)
+
+        self.origen_lineEdit = QLineEdit(self.groupBox)
+        self.origen_lineEdit.setObjectName(u"origen_lineEdit")
+
+        self.gridLayout.addWidget(self.origen_lineEdit, 1, 1, 1, 1)
+
+        self.label_4 = QLabel(self.groupBox)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout.addWidget(self.label_4, 2, 0, 1, 1)
+
+        self.peso_spinBox = QSpinBox(self.groupBox)
+        self.peso_spinBox.setObjectName(u"peso_spinBox")
+
+        self.gridLayout.addWidget(self.peso_spinBox, 3, 1, 1, 1)
 
 
-        self.verticalLayout_4.addWidget(self.widget)
+        self.gridLayout_3.addWidget(self.groupBox, 0, 0, 1, 1)
 
-
-        self.horizontalLayout.addWidget(self.widget_3)
-
-        self.salida = QPlainTextEdit(self.groupBox)
+        self.salida = QPlainTextEdit(self.tab)
         self.salida.setObjectName(u"salida")
-        self.salida.setEnabled(True)
-        sizePolicy1.setHeightForWidth(self.salida.sizePolicy().hasHeightForWidth())
-        self.salida.setSizePolicy(sizePolicy1)
-        self.salida.setContextMenuPolicy(Qt.DefaultContextMenu)
-        self.salida.setAcceptDrops(True)
-        self.salida.setReadOnly(True)
 
-        self.horizontalLayout.addWidget(self.salida)
+        self.gridLayout_3.addWidget(self.salida, 0, 1, 1, 1)
 
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.gridLayout_4 = QGridLayout(self.tab_2)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.buscar_pushButton = QPushButton(self.tab_2)
+        self.buscar_pushButton.setObjectName(u"buscar_pushButton")
 
-        self.horizontalLayout_5.addWidget(self.groupBox)
+        self.gridLayout_4.addWidget(self.buscar_pushButton, 2, 1, 1, 1)
+
+        self.buscar_lineEdit = QLineEdit(self.tab_2)
+        self.buscar_lineEdit.setObjectName(u"buscar_lineEdit")
+
+        self.gridLayout_4.addWidget(self.buscar_lineEdit, 2, 0, 1, 1)
+
+        self.mostrar_tabla_pushButton = QPushButton(self.tab_2)
+        self.mostrar_tabla_pushButton.setObjectName(u"mostrar_tabla_pushButton")
+
+        self.gridLayout_4.addWidget(self.mostrar_tabla_pushButton, 2, 2, 1, 1)
+
+        self.tabla = QTableWidget(self.tab_2)
+        self.tabla.setObjectName(u"tabla")
+
+        self.gridLayout_4.addWidget(self.tabla, 1, 0, 1, 3)
+
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.gridLayout_2.addWidget(self.tabWidget, 0, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 600, 22))
+        self.menubar.setGeometry(QRect(0, 0, 613, 21))
         self.menuArchivo = QMenu(self.menubar)
         self.menuArchivo.setObjectName(u"menuArchivo")
         MainWindow.setMenuBar(self.menubar)
@@ -158,6 +145,9 @@ class Ui_MainWindow(object):
         self.menuArchivo.addAction(self.actionGuardar)
 
         self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(1)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -173,14 +163,19 @@ class Ui_MainWindow(object):
         self.actionGuardar.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+S", None))
 #endif // QT_CONFIG(shortcut)
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Vuelo", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"id:", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Origen:", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Detino:", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Peso:", None))
-        self.agregar_final_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar Final", None))
         self.agregar_inicio_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar Inicio", None))
+        self.agregar_final_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar Final", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"id:", None))
         self.mostrar_pushButton.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Detino:", None))
         self.salida.setPlainText("")
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Agregar", None))
+        self.buscar_pushButton.setText(QCoreApplication.translate("MainWindow", u"Buscar", None))
+        self.buscar_lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Identificador de vuelo", None))
+        self.mostrar_tabla_pushButton.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tabla", None))
         self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
     # retranslateUi
 
